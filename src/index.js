760262da5bf8d5472a2c0652e7b378893516d96d
romulofull <romulo.castro@gmail.com>
2024-08-13 15:52:41 -0500

    Saludos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello World</h1>)
+//root.render(<h1>Hello Bonito Mundo</h1>)
+
+function Greeting() {
+    return <div>
+    <h1>Hola Mundo</h1>
+    <p>lorem123</p></div>
+}
+
+//-root.render(Greeting())
+
+root.render(<div>  {Greeting()}
+    {Greeting()}
+    <Greeting></Greeting>
+    <Greeting />
+
+</div>)
