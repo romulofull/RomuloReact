f152e5e9e535c8b8abe1dff848fc7fb02de98797
romulofull <romulo.castro@gmail.com>
2024-08-13 16:35:41 -0500

    Condicionales en React

--- src/index.js
+++ src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 //root.render(<h1>Hello Bonito Mundo</h1>)
 
-function Greeting() {
+/*function Greeting() {
     return <div>
     <h1>Hola Mundo</h1>
     <p>lorem123</p></div>
-}
+}*/
 
-//-root.render(Greeting())
+//root.render(Greeting())
 
-root.render(<div>  {Greeting()}
+/*root.render(<div>  {Greeting()}
     {Greeting()}
     <Greeting></Greeting>
     <Greeting />
 
-</div>)
+</div>)*/
+
+/*function Greetingname() {
+    const name = 'Romulo'
+    return <h1>{10 +20}</h1>
+    //return <h1> name </h1>
+}
+root.render(Greetingname())*/
+
+
+function Greetingnames() {
+    const married = false
+ /*   if (married) {
+        return <h1>Estoy casado</h1>
+     } else {return <h1>No estoy casado</h1>
+}}*/
+
+
+return <h1> {married ? 'estoy casado': "no estoy casado"}</h1>}
+
+root.render(Greetingnames())
