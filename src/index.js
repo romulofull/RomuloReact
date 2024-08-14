8bfc51b5afeeae5495826b9400202b932b90e4ac
romulofull <romulo.castro@gmail.com>
2024-08-13 22:52:47 -0500

    JSX EN REACT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(<h1>Hello Bonito Mundo</h1>)
 
 /*function Greeting() {
@@ -25,15 +25,78 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 }
 root.render(Greetingname())*/
 
-
-function Greetingnames() {
-    const married = false
- /*   if (married) {
+/*function Greetingnames() {
+  const married = false;
+  /*   if (married) {
         return <h1>Estoy casado</h1>
      } else {return <h1>No estoy casado</h1>
-}}*/
+}}
+
+  return <h1> {married ? "estoy casado 😃" : "no estoy casado🤫"}</h1>;
+}
+
+root.render(Greetingnames());*/
+
+/* function Greetingnames() {
+  const user = {
+    firstName: "ryan",
+    lastName: "Ray",
+  };
+
+  return <h1> { JSON.stringify(user)  }</h1>;
+}
+
+root.render(Greetingnames());*/
+
+/*function Greetingnames() {
+    const user = {
+      firstName: "Rom",
+      lastName: "RomC",
+    };
+  
+    return (<div><h1>{user.firstName}</h1>
+    <h3>{user.lastName}</h3>
+    </div>);
+}
+  
+  root.render(Greetingnames());*/
+
+/*function Greetingnames() {
+  const married = true;
+  return (
+    <div>
+      <h1> {married.toString()}</h1>
+    </div>
+  );
+}
+root.render(Greetingnames());*/
+
+/*function Greetingnames() {
+  function add(x,y) {
+    return x+y}
+  return (
+    <div>
+      <h1>{add(10,30)}</h1>
+    </div>
+  );
+
+root.render(<div><Greetingnames/>
+<Greetingnames/>
+<Greetingnames/>
+<Greetingnames/>
+<Greetingnames/></div>);}*/
+
+function Greetingnames() {
+  function add(x,y) {
+    return x+y;}
+  return (
+      <h1>{add(10,30)}</h1>);
+}
+root.render(<><Greetingnames/>
+<Greetingnames/>
+<Greetingnames/>
+<Greetingnames/>
+<Greetingnames/></>);
 
 
-return <h1> {married ? 'estoy casado': "no estoy casado"}</h1>}
 
-root.render(Greetingnames())
